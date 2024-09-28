2e7a5939965b6593a6725c76a25c50a213dc4316
MilindTajane143 <90783629+MilindTajane143@users.noreply.github.com>
2024-09-28 11:28:00 +0530

    done

--- Python_Practical/largest_no.py
+++ Python_Practical/largest_no.py
@@ -1,3 +1,5 @@
+# OWN CODE
+
 num1 = int(input("Enter 1st parameter"))
 num2 = int(input("Enter 2nd parameter"))
 num3 = int(input("Enter 3rd parameter"))
@@ -8,3 +10,18 @@ elif (num2 > num3):
     print ("Num2 is greater than num 1 and num 3")
 elif (num3 > num1):
     print ("Num3 is greater than num 1 and num2")
+
+# Manual Code
+
+num1 = float(input("Enter 1st parameter: "))
+num2 = float(input("Enter 2nd parameter: "))
+num3 = float(input("Enter 3rd parameter: "))
+
+if (num1 >= num2) and (num1 >= num3):
+   largest = num1
+elif (num2 >= num1) and (num2 >= num3):
+   largest = num2
+else:
+   largest = num3
+
+print("The largest number is", largest)
