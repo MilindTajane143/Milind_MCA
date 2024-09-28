513fb1b13685f76247b0e44955fffda9032fa432
MilindTajane143 <90783629+MilindTajane143@users.noreply.github.com>
2024-09-28 11:04:32 +0530

    done

---
+++ Python_Practical/else-if_statement.py
@@ -0,0 +1,31 @@
+letter = "A"
+if letter == "B":
+    print("The letter is B")
+elif letter == "C":
+    print("The letter is C")
+elif letter == "A":
+    print("The letter is A")
+else:
+    print("The letter is not A, B, or C")
+
+
+letter = "B"
+if letter == "B":
+    print("The letter is B")
+elif letter == "C":
+    print("The letter is C")
+elif letter == "A":
+    print("The letter is A")
+else:
+    print("The letter is not A, B, or C")
+
+    
+letter = "C"
+if letter == "B":
+    print("The letter is B")
+elif letter == "C":
+    print("The letter is C")
+elif letter == "A":
+    print("The letter is A")
+else:
+    print("The letter is not A, B, or C")
