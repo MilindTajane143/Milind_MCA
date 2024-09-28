6de781039cbd4296e284724207cbad7cd80e3189
MilindTajane143 <90783629+MilindTajane143@users.noreply.github.com>
2024-09-28 10:44:55 +0530

    done

---
+++ Python_Practical/swap_two_numbers.py
@@ -0,0 +1,8 @@
+x = input('Enter value of x: ')
+y = input('Enter value of y: ')
+temp = x
+x = y
+y = temp
+
+print('Value of x after swapping {}'.format(x))
+print('Value of Y after swapping {}'.format(y))
