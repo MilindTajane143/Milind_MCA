392a52b2f41dc2e513e800f3aad5f8d2f2057707
MilindTajane143 <90783629+MilindTajane143@users.noreply.github.com>
2024-09-28 11:24:51 +0530

    greate no

---
+++ Python_Practical/largest_no.py
@@ -0,0 +1,10 @@
+num1 = int(input("Enter 1st parameter"))
+num2 = int(input("Enter 2nd parameter"))
+num3 = int(input("Enter 3rd parameter"))
+
+if (num1 > num2):
+    print ("Num1 is greater than num 2 and num 3 ")
+elif (num2 > num3):
+    print ("Num2 is greater than num 1 and num 3")
+elif (num3 > num1):
+    print ("Num3 is greater than num 1 and num2")
