b5ce5e9c3c13ca2b3b1f3b111b9fcc7e5b03811d
MilindTajane143 <90783629+MilindTajane143@users.noreply.github.com>
2024-09-30 14:20:49 +0530

    done

--- Python_Practical/break_statement.py
+++ Python_Practical/break_statement.py
@@ -1,4 +1,21 @@
-for i in range(50):
-    if i == 25:
-        break
-    print(i)
+# Manual Code
+# for i in range(50):
+#     if i == 25:
+#         break
+#     print(i)
+
+# Own Code
+# Print prime numbers
+
+lower = 0
+upper = 100
+
+print("Prime numbers between", lower, "and", upper, "are:")
+
+for num in range(lower, upper + 1):
+   if num > 1:
+       for i in range(2, num):
+           if (num % i) == 0:
+               break
+       else:
+           print(num)
