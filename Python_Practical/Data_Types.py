7f052b18eecf3897c9664e3c642d4b3cb5fe5294
MilindTajane143 <milindtajane1@gmail.com>
2024-10-06 22:29:23 +0530

    done

---
+++ Python_Practical/Data_Types.py
@@ -0,0 +1,18 @@
+a = 2
+b = 0.3
+c = 'Hello World'
+d = [10, 3,'Hi']
+e = True
+f = (10, 3, 'hi')
+g = {'Delhi':30.2, 'Bihar':29, 'Karnataka':26}
+h = set(d)
+i = None
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
+print(type(h))
+print(type(i))
