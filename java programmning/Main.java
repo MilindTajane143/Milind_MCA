6e8dbc5695e31d69497461bc4d10556fc834defa
MilindTajane143 <90783629+MilindTajane143@users.noreply.github.com>
2025-02-13 15:21:51 +0530

    done

---
+++ java programmning/Main.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the radius of circle: ");
+        float radius = sc.nextFloat();
+        if (radius <=0){
+            System.out.println("Radius can not be less tha 1 to calculate area of circle");
+        }
+        else
+        {
+            int nu=22, de=7;
+            float area = (float)nu/de*radius*radius;
+        System.out.println("Area of circle is " + area +" sq. units");
+        }
+    }
+}
